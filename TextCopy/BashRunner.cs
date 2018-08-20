5ce6ed34c9838eca0b8d8af426aaeff4f4941a92
Simon Cropp <simon.cropp@gmail.com>
2018-08-20 20:23:19 +1000

    .

--- TextCopy/BashRunner.cs
+++ TextCopy/BashRunner.cs
@@ -1,10 +1,13 @@
 using System;
 using System.Diagnostics;
+using System.Text;
 
 static class BashRunner
 {
     public static string Run(string commandLine)
     {
+        var errorBuilder = new StringBuilder();
+        var outputBuilder = new StringBuilder();
         using (var process = new Process
         {
             StartInfo = new ProcessStartInfo
@@ -12,20 +15,25 @@ static class BashRunner
                 FileName = "/bin/bash",
                 Arguments = $"-c \"{commandLine}\"",
                 RedirectStandardOutput = true,
+                RedirectStandardError = true,
                 UseShellExecute = false,
                 CreateNoWindow = false,
             }
         })
         {
+            process.ErrorDataReceived += (sender, args) => { errorBuilder.AppendLine(args.Data); };
+            process.OutputDataReceived += (sender, args) => { outputBuilder.AppendLine(args.Data); };
             process.Start();
-            var result = process.StandardOutput.ReadToEnd();
             process.WaitForExit();
-            if (process.ExitCode != 0)
+            if (process.ExitCode == 0)
             {
-                var error = $"Could not execute process. Command line: {commandLine}.{Environment.NewLine}Output:{result}";
-                throw new Exception(error);
+                return outputBuilder.ToString();
             }
-            return result;
+
+            var error = $@"Could not execute process. Command line: {commandLine}.
+Output: {outputBuilder}
+Error: {errorBuilder}";
+            throw new Exception(error);
         }
     }
 }
