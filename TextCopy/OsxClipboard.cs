a313deb03064821f2c8ec484f66343a18d15a4ff
Simon Cropp <simon.cropp@gmail.com>
2018-08-23 21:17:56 +1000

    better cleanup

--- TextCopy/OsxClipboard.cs
+++ TextCopy/OsxClipboard.cs
@@ -3,57 +3,60 @@ using System.Runtime.InteropServices;
 
 static class OsxClipboard
 {
+    static IntPtr nsString;
+    static IntPtr generalPasteboard;
+    static IntPtr utfDataType;
+    static IntPtr dataType;
+
+    static OsxClipboard()
+    {
+        nsString = objc_getClass("NSString");
+        var nsPasteboard = objc_getClass("NSPasteboard");
+        generalPasteboard = objc_msgSend(nsPasteboard, sel_registerName("generalPasteboard"));
+        utfDataType = objc_msgSend(objc_msgSend(nsString, sel_registerName("alloc")), sel_registerName("initWithUTF8String:"), "public.utf8-plain-text");
+
+        dataType = objc_msgSend(objc_msgSend(nsString, sel_registerName("alloc")), sel_registerName("initWithUTF8String:"), "NSStringPboardType");
+    }
+
     public static string GetText()
     {
-        var nsString = objc_getClass("NSString");
-        IntPtr dataType = default;
+        IntPtr ptr = default;
+        IntPtr charArray = default;
         try
         {
-            dataType = objc_msgSend(objc_msgSend(nsString, sel_registerName("alloc")), sel_registerName("initWithUTF8String:"), "NSStringPboardType");
-
-            var nsPasteboard = objc_getClass("NSPasteboard");
-            var generalPasteboard = objc_msgSend(nsPasteboard, sel_registerName("generalPasteboard"));
-
-            var ptr = objc_msgSend(generalPasteboard, sel_registerName("stringForType:"), dataType);
-            var charArray = objc_msgSend(ptr, sel_registerName("UTF8String"));
+            ptr = objc_msgSend(generalPasteboard, sel_registerName("stringForType:"), dataType);
+            charArray = objc_msgSend(ptr, sel_registerName("UTF8String"));
 
             return Marshal.PtrToStringAnsi(charArray);
         }
         finally
         {
-            if (dataType != default)
+            if (ptr != default)
             {
-                objc_msgSend(dataType, sel_registerName("release"));
+                objc_msgSend(ptr, sel_registerName("release"));
+            }
+            if (charArray != default)
+            {
+                objc_msgSend(charArray, sel_registerName("release"));
             }
         }
     }
 
     public static void SetText(string text)
     {
-        var nsString = objc_getClass("NSString");
         IntPtr str = default;
-        IntPtr dataType = default;
         try
         {
             str = objc_msgSend(objc_msgSend(nsString, sel_registerName("alloc")), sel_registerName("initWithUTF8String:"), text);
-            dataType = objc_msgSend(objc_msgSend(nsString, sel_registerName("alloc")), sel_registerName("initWithUTF8String:"), NSPasteboardTypeString);
-
-            var nsPasteboard = objc_getClass("NSPasteboard");
-            var generalPasteboard = objc_msgSend(nsPasteboard, sel_registerName("generalPasteboard"));
 
             objc_msgSend(generalPasteboard, sel_registerName("clearContents"));
-            objc_msgSend(generalPasteboard, sel_registerName("setString:forType:"), str, dataType);
+            objc_msgSend(generalPasteboard, sel_registerName("setString:forType:"), str, utfDataType);
         }
         finally
         {
             if (str != default)
             {
                 objc_msgSend(str, sel_registerName("release"));
-            }
-
-            if (dataType != default)
-            {
-                objc_msgSend(dataType, sel_registerName("release"));
             }
         }
     }
@@ -69,11 +72,10 @@ static class OsxClipboard
 
     [DllImport("/System/Library/Frameworks/AppKit.framework/AppKit")]
     static extern IntPtr objc_msgSend(IntPtr receiver, IntPtr selector, IntPtr arg1);
+
     [DllImport("/System/Library/Frameworks/AppKit.framework/AppKit")]
     static extern IntPtr objc_msgSend(IntPtr receiver, IntPtr selector, IntPtr arg1, IntPtr arg2);
 
     [DllImport("/System/Library/Frameworks/AppKit.framework/AppKit")]
     static extern IntPtr sel_registerName(string selectorName);
-
-    const string NSPasteboardTypeString = "public.utf8-plain-text";
 }
