6785826ccb8c07d54fddf4c46989999a18490ae3
Simon Cropp <simon.cropp@gmail.com>
2019-08-01 19:56:02 +1000

    support lower framework

---
+++ src/TextCopy/netdesktop/Clipboard.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace TextCopy
+{
+    public static class Clipboard
+    {
+        static Action<string> setAction = CreateSet();
+        static Func<string> getFunc = CreateGet();
+
+        public static void SetText(string text)
+        {
+            if (text == null)
+            {
+                throw new ArgumentNullException(nameof(text));
+            }
+
+            setAction(text);
+        }
+
+        public static string GetText()
+        {
+            return getFunc();
+        }
+
+        static Action<string> CreateSet()
+        {
+            return WindowsClipboard.SetText;
+        }
+
+        static Func<string> CreateGet()
+        {
+            return WindowsClipboard.GetText;
+        }
+    }
+}
