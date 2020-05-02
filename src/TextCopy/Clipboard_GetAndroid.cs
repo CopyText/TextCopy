23dce640c2676dac5fbea8c55c4d9c623f52505f
Martin Finkel <finkel2804@gmail.com>
2020-05-02 13:38:31 +0200

    TextCopy: Add Xamarin.Android support

---
+++ src/TextCopy/Clipboard_GetAndroid.cs
@@ -0,0 +1,30 @@
+﻿#if ANDROID
+using System;
+using System.Threading.Tasks;
+using System.Threading;
+using Android.Content;
+
+namespace TextCopy
+{
+    public static partial class Clipboard
+    {
+        static Func<CancellationToken, Task<string?>> CreateAsyncGet()
+        {
+            return (token) => Task.FromResult<string?>(GetTextAndroid());
+        }
+
+        static Func<string?> CreateGet()
+        {
+            var func = CreateAsyncGet();
+            return () => func(CancellationToken.None).GetAwaiter().GetResult();
+        }
+
+        static string? GetTextAndroid()
+        {
+            var context = Android.App.Application.Context;
+            if (context is null) return null;
+            return ClipboardManager.FromContext(context).Text;
+        }
+    }
+}
+#endif
