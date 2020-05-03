6a053b3e4c74279ec8d0f6d0466ed9f1268b2759
Martin Finkel <finkel2804@gmail.com>
2020-05-03 19:58:58 +0200

    Add iOS support

---
+++ src/TextCopy/Clipboard_GetiOS.cs
@@ -0,0 +1,27 @@
+﻿#if IOS
+using System;
+using System.Threading.Tasks;
+using System.Threading;
+using UIKit;
+
+namespace TextCopy
+{
+    public static partial class Clipboard
+    {
+        static Func<CancellationToken, Task<string?>> CreateAsyncGet()
+        {
+            return token => Task.FromResult(GetTextiOS());
+        }
+
+        static Func<string?> CreateGet()
+        {
+            return GetTextiOS;
+        }
+
+        static string? GetTextiOS()
+        {
+            return UIPasteboard.General.String;
+        }
+    }
+}
+#endif
