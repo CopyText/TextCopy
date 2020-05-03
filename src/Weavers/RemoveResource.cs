9ff565067261430ee8071c35335c8569d3655b6f
Simon Cropp <simon.cropp@gmail.com>
2020-05-03 17:41:09 +1000

    remove resources (#177)

---
+++ src/Weavers/RemoveResource.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Fody;
+
+public class ModuleWeaver :
+    BaseModuleWeaver
+{
+    public override void Execute()
+    {
+        var types = ModuleDefinition.Types;
+        var resourceType = types.SingleOrDefault(x => x.Name == "Resource");
+        if (resourceType != null)
+        {
+            types.Remove(resourceType);
+        }
+    }
+
+    public override IEnumerable<string> GetAssembliesForScanning()
+    {
+        yield break;
+    }
+}
