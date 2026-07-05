5a9b1c10e6b35637c1118ff3c52ac5faa812edf0
Simon Cropp <simon.cropp@gmail.com>
2026-07-05 10:16:38 +1000

    .

--- src/build.ps1
+++ src/build.ps1
@@ -1,7 +1,28 @@
-msbuild.exe src/TextCopy.sln /t:restore /p:Configuration=Release -verbosity:quiet
+$ErrorActionPreference = 'Stop'
 
-msbuild.exe src/TextCopy.sln /t:build /p:Configuration=Release -verbosity:quiet
+function exec($command) {
+    & $command
+    if ($LastExitCode -ne 0) {
+        throw "Failed with exit code $($LastExitCode): $command"
+    }
+}
 
-msbuild.exe src/TextCopy.sln /t:pack /p:Configuration=Release -verbosity:quiet
-
-dotnet test src --configuration Release --no-build --no-restore
+if ($env:OS -eq 'Windows_NT') {
+    # full MSBuild is required for the Xamarin targets and the legacy Android/iOS projects
+    exec { msbuild.exe src/TextCopy.sln /t:restore /p:Configuration=Release -verbosity:quiet }
+    exec { msbuild.exe src/TextCopy.sln /t:build /p:Configuration=Release -verbosity:quiet }
+    exec { msbuild.exe src/TextCopy.sln /t:pack /p:Configuration=Release -verbosity:quiet }
+    exec { dotnet test src --configuration Release --no-build --no-restore }
+}
+else {
+    # the Xamarin targets and the legacy Android/iOS projects cannot build on the dotnet CLI,
+    # so build only what the tests need and run the net6.0 tests against the OS clipboard
+    exec { dotnet build src/Weavers --configuration Release }
+    if ($IsLinux) {
+        # xsel requires an X display
+        exec { xvfb-run --auto-servernum dotnet test src/Tests --configuration Release --framework net6.0 }
+    }
+    else {
+        exec { dotnet test src/Tests --configuration Release --framework net6.0 }
+    }
+}
